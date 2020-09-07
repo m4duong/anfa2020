20820864287b19ee5ca7ddb5d483bdee4528e800
m4duong <51769067+m4duong@users.noreply.github.com>
2020-09-06 20:27:49 -0700

    Add files via upload
    start

---
+++ js/subMenu.js
@@ -0,0 +1,13 @@
+function openMenu(evt, menuName) {
+  var i, tabcontent, submenus;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
